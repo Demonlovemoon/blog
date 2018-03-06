d03e2735aca413ae28163284f217dec440af0cbd
Demonlovemon <xuyuexi890719@163.com>
2018-03-06 14:53:54 +0800

    练练手

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,12 @@
+class ArticlesController < ApplicationController
+
+  def new
+    puts 'this is new'
+
+  end
+
+  def create
+    puts 'this is create'
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  root 'welcome#index'#不是路径
+
+  get 'welcome/index'
+  #是路径
+
+  resources :articles
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
