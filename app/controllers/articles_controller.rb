773bbf99977af1cbe75a011019cfec28a55e5c1c
Demonlovemon <xuyuexi890719@163.com>
2018-03-06 16:18:31 +0800

    s

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,26 @@
 class ArticlesController < ApplicationController
 
-  def new
-    puts 'this is new'
+  def index
+    @articles = Article.all
+  end
 
+  def new
   end
 
   def create
-    puts 'this is create'
+    @article = Article.new(article_param)
+    @article.save
+    redirect_to @article #相当于 redirect_to 'show'
+    #redirect_to 'index'
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  private
+  def article_param
+    params.require(:article).permit(:title, :text)
   end
 
 end
