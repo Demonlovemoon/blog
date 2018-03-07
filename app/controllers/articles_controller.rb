1ec46e03df0a80486cc6e275d91fe3be6013a503
Demonlovemon <xuyuexi890719@163.com>
2018-03-07 18:40:38 +0800

    写代码好开心

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,18 +5,49 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
   def create
     @article = Article.new(article_param)
-    @article.save
-    redirect_to @article #相当于 redirect_to 'show'
-    #redirect_to 'index'
+
+    if @article.save
+
+      redirect_to @article #相当于 redirect_to 'show'
+    else
+     render 'new'
+    end
+
+      #redirect_to 'index'
   end
 
   def show
     @article = Article.find(params[:id])
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_param)
+
+      redirect_to @article #相当于 redirect_to 'show'
+    else
+      render 'edit'
+    end
+  end
+
+   def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
+
+
 
   private
   def article_param
